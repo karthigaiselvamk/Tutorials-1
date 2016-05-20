c474ba7ff441a361a1fc339ad0ecaa78a96095fd
Magnus Ringkjøb <magnus@ringkjob.com>
2016-05-20 18:40:31 +0200

    backup

---
+++ Chrome Apps/codelab/background.js
@@ -0,0 +1,8 @@
+/**
+ * Listens for the app launching then creates the window
+ * 
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+    
+})
