409f7f78be5070f585b7d2efa455db25ac90ed5d
Magnus Ringkjøb <magnus@ringkjob.com>
2016-05-20 19:33:12 +0200

    Finishet #1

--- Chrome Apps/codelab/background.js
+++ Chrome Apps/codelab/background.js
@@ -3,6 +3,9 @@
  * 
  * @see http://developer.chrome.com/apps/app.window.html
  */
-chrome.app.runtime.onLaunched.addListener(function() {
-    
+chrome.app.runtime.onLaunched.addListener(function () {
+    chrome.app.window.create('index.html', {
+        id: 'main',
+        bounds: { width: 620, height: 500 }
+    })
 })
