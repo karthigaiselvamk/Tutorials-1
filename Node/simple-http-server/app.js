93fd22502a74a179933e5ff241df6a240034a3e9
Magnus Ringkjøb <magnus@ringkjob.com>
2016-05-31 23:40:17 +0200

    Backup

--- Node/simple-http-server/app.js
+++ Node/simple-http-server/app.js
@@ -1,22 +1,59 @@
-var http = require('http')
-var fs = require('fs')
+/**
+ * https://developer.mozilla.org/en-US/docs/Node_server_without_framework
+ */
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
-// 404
-function send404Response(response) {
-    response.writeHead(404, {"Content-Type": "text/plain"})
-    response.write("404 not found")
-    response.end()
+http.createServer(function (request, response) {
+console.log('request starting...');
+
+var filePath = '.' + request.url;
+if (filePath == './')
+    filePath = './index.html';
+
+var extname = path.extname(filePath);
+var contentType = 'text/html';
+switch (extname) {
+    case '.js':
+        contentType = 'text/javascript';
+        break;
+    case '.css':
+        contentType = 'text/css';
+        break;
+    case '.json':
+        contentType = 'application/json';
+        break;
+    case '.png':
+        contentType = 'image/png';
+        break;      
+    case '.jpg':
+        contentType = 'image/jpg';
+        break;
+    case '.wav':
+        contentType = 'audio/wav';
+        break;
 }
 
-function onRequest(request, response) {
-    if(request.method == 'GET' && request.url == '/') {
-        response.writeHead(200, {"Content-Type": "text/html"})
-        fs.createReadStream('./public/index.html').pipe(response)
+fs.readFile(filePath, function(error, content) {
+    if (error) {
+        if(error.code == 'ENOENT'){
+            fs.readFile('./404.html', function(error, content) {
+                response.writeHead(200, { 'Content-Type': contentType });
+                response.end(content, 'utf-8');
+            });
+        }
+        else {
+            response.writeHead(500);
+            response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+            response.end(); 
+        }
     }
     else {
-        send404Response(response)
+        response.writeHead(200, { 'Content-Type': contentType });
+        response.end(content, 'utf-8');
     }
-}
+});
 
-http.createServer(onRequest).listen(8080);
-console.log("Web server is ...")
+}).listen(8125);
+console.log('Server running at http://127.0.0.1:8125/');
---
+++ Node/simple-http-server/script.js
@@ -0,0 +1,8 @@
+var a = 1
+var b = 2
+var sum = a + b
+
+var mathDiv = document.createElement('div')
+mathDiv.innerText = a + " + " + b + " = " + sum
+
+document.getElementsByTagName('body')[0].appendChild(mathDiv);
