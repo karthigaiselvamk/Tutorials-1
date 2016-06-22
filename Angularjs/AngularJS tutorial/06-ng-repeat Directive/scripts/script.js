457bc9285ba3b28a54da4963fcf8079c8bef09c3
Magnus Ringkjøb <magnus@ringkjob.com>
2016-06-22 19:44:21 +0200

    05 & 06

---
+++ Angularjs/AngularJS tutorial/06-ng-repeat Directive/scripts/script.js
@@ -0,0 +1,35 @@
+/// <reference path="angular.min.js" />
+
+var myApp = angular
+    .module("myModule", [])
+    .controller("myController", function($scope) {
+
+       var countries = [
+           {
+               name: "UK",
+               cities: [
+                   {name: "London"},
+                   {name: "Manchester"},
+                   {name: "Birmingham"}
+               ]
+           },
+           {
+               name: "Norway",
+               cities: [
+                   {name: "Oslo"},
+                   {name: "Bergen"},
+                   {name: "Trondheim"}
+               ]
+           },
+           {
+               name: "USA",
+               cities: [
+                   {name: "Washington"},
+                   {name: "San Diego"},
+                   {name: "Los Angeles"}
+               ]
+           },
+       ]
+
+       $scope.countries = countries;
+    });
