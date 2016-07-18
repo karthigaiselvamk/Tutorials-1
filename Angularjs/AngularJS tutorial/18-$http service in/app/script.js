d11b82867c5c1f5990401b3b0a4d24689cea9ae5
Magnus Ringkjøb <magnus@ringkjob.com>
2016-07-18 11:12:12 +0200

    Tut 18

---
+++ Angularjs/AngularJS tutorial/18-$http service in/app/script.js
@@ -0,0 +1,20 @@
+/// <reference path="bower_components/angular/angular.js" />
+
+var app = angular
+  .module("myModule", [])
+  .controller("myController", ($scope, $http, $log) => {
+
+    $http({
+      method: 'GET',
+      url: 'data.json'
+    }).then(successCallBack, errorCallBack);
+
+    function successCallBack(response) {
+      $scope.employees = response.data;
+    }
+
+    function errorCallBack(response) {
+      $scope.error = response.data;
+    }
+    
+  });
